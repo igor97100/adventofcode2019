d319886eeb53b0d77f6ff84c1484f7ff2abd8a15
IgorSpi <igor13pao@hotmail.com>
2019-12-03 01:42:59 +0100

    changed the name of the calc_mass function to calc_fuel because it actually calculates the fuel based on the mass of the module

--- day_1/day_1_solution.py
+++ day_1/day_1_solution.py
@@ -2,13 +2,12 @@ import numpy as np
 
 def main():
     input_np = np.loadtxt(fname = "input")
-    print(input_np)
-    masses = calc_mass(input_np) 
+    fuels = calc_mass(input_np) 
     print(masses) 
 
-def calc_mass(x):
-    mass = x // 3 - 2 
-    return mass
+def calc_fuel(x):
+    fuel = x // 3 - 2 
+    return fuel
 
 if __name__ == '__main__':
     main()

a6e2c3cadd4842806cd30fdfc39276c0d773613f
IgorSpi <igor13pao@hotmail.com>
2019-12-03 03:51:58 +0100

    found the correct answer using a recursive function, maping this function to the input and summing up the results

--- day_1/day_1_b_solution.py
+++ day_1/day_1_b_solution.py
@@ -2,10 +2,11 @@ import numpy as np
 
 def main():
     input_np = np.loadtxt(fname = "input")
-    #fuels = calc_fuel(input_np)
+    fuels = [calc_fuel(x) for x in input_np]
+
     print(calc_fuel(12))
     print(calc_fuel(1969))
-    #print(np.sum(fuels))
+    print(np.sum(fuels))
 
 def calc_fuel(x, fuels = np.array([])):
     fuel = x // 3 - 2 

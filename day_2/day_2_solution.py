cf661f81508a1d88a9a70986d42dfc870ef98c75
IgorSpi <igor13pao@hotmail.com>
2019-12-03 05:20:01 +0100

    almost here but the output is incorrect

--- day_2/day_2_solution.py
+++ day_2/day_2_solution.py
@@ -1,8 +1,22 @@
 import numpy as np
 
 def main():
-  seq = np.loadtxt(fname = 'input', delimiter = ',', dtype= np.uint16)
-  print(seq[0])
+    seq = np.loadtxt(fname = 'input', delimiter = ',', dtype= np.uint16)
+    seq[1] = 12
+    seq[2] = 2
+    
+    for i in range(0, len(seq), 4):
+        opcode = seq[i]
+        if opcode == 99:
+            break
+        elif opcode == 1:
+            seq[seq[i+3]] = seq[i+1] + seq[i+2]
+        elif opcode == 2:
+            seq[seq[i+3]] = seq[i+1] * seq[i+2]
+        else:
+            print(f'opcode {opcode} at position {i} is invalid')
+    
+    print(seq[0])
 
 if __name__ == '__main__':
     main()

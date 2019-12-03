439c7df4244a12e53065379d9a5529fea07e1c03
IgorSpi <igor13pao@hotmail.com>
2019-12-03 05:35:37 +0100

    found the correct solution for the first part of the day 2 puzzle

--- day_2/day_2_solution.py
+++ day_2/day_2_solution.py
@@ -1,20 +1,23 @@
 import numpy as np
 
 def main():
-    seq = np.loadtxt(fname = 'input', delimiter = ',', dtype= np.uint16)
+    seq = np.loadtxt(fname = 'input', delimiter = ',', dtype= np.uint64)
     seq[1] = 12
     seq[2] = 2
-    
+    print(seq) 
     for i in range(0, len(seq), 4):
+
         opcode = seq[i]
         if opcode == 99:
             break
         elif opcode == 1:
-            seq[seq[i+3]] = seq[i+1] + seq[i+2]
+            seq[seq[i+3]] = seq[seq[i+1]] + seq[seq[i+2]]
+            print(f'seq[{i+3}] became {seq[i+3]}')
         elif opcode == 2:
-            seq[seq[i+3]] = seq[i+1] * seq[i+2]
+            seq[seq[i+3]] = seq[seq[i+1]] * seq[seq[i+2]]
         else:
             print(f'opcode {opcode} at position {i} is invalid')
+            break
     
     print(seq[0])
 

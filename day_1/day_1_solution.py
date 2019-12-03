c2fb1e5ed412606b0fb8fe893754ad391b204a72
IgorSpi <igor13pao@hotmail.com>
2019-12-03 01:49:23 +0100

    completed the puzzle the main prints the solution

--- day_1/day_1_solution.py
+++ day_1/day_1_solution.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def main():
     input_np = np.loadtxt(fname = "input")
-    fuels = calc_mass(input_np) 
-    print(masses) 
+    fuels = calc_fuel(input_np)
+    print(np.sum(fuels))
 
 def calc_fuel(x):
     fuel = x // 3 - 2 

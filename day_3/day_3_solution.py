3493f8eb37fb75db851fc25b2f264d7fdab47b8d
IgorSpi <igor13pao@hotmail.com>
2019-12-06 00:51:45 +0100

    done the import and the proccessing of the input. Each wire is a list of directions

---
+++ day_3/day_3_solution.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main():
+    with open('input', 'r') as f:
+        wires = f.read().splitlines()
+        wire0 = wires[0].split(',')
+        wire1 = wires[1].split(',')
+if __name__ == "__main__":
+    main()

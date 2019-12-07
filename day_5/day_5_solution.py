7975a52c41e5b315fd3cdcb8d1720bb25d2357e2
IgorSpi <igor13pao@hotmail.com>
2019-12-07 03:59:21 +0100

    solved first part

--- day_5/day_5_solution.py
+++ day_5/day_5_solution.py
@@ -7,7 +7,9 @@ class Error(Exception):
 
 def main():
     input_ = np.loadtxt(fname = 'input', delimiter = ',', dtype=np.int64)
-
+    print
+    input_ = np.concatenate((input_, np.zeros(1000, dtype = np.int64)))    
+    run_intcode(input_, 1)
 
 def run_intcode(original_seq, only_input):
     seq = np.copy(original_seq)
@@ -17,14 +19,29 @@ def run_intcode(original_seq, only_input):
         if opcode == 3:
             seq[seq[i+1]] = only_input
             i += 2
+            
         elif opcode == 4:
-            print(f'Opcode 4 output: {seq[seq[i+1]]}}')
+            print(f'Opcode 4 output: {seq[seq[i+1]]}')
             i += 2
         else:
 
-            unmerged_opcode = map(int, str(opcode))
-            while len(unmerged_opcode != 5):
+            unmerged_opcode = list(map(int, str(opcode)))
+            while len(unmerged_opcode) != 5:
                 unmerged_opcode.insert(0, 0)
+            print(unmerged_opcode)
+            param1 = seq[seq[i+1]] if unmerged_opcode[2] == 0 else seq[i+1]
+            param2 = seq[seq[i+2]] if unmerged_opcode[1] == 0 else seq[i+2]
+            param3 = seq[i+3] if unmerged_opcode[0] == 0 else i+3
+            if unmerged_opcode[4] == 1:
+                seq[param3] = param1 + param2
+                print(param1, param2, param3)
+            elif unmerged_opcode[4] == 2:
+                seq[param3] = param1 * param2
+            else:
+                print(i, opcode)
+                print('ERROR !! Not recognized opcode')
+            i+= 4
+
         opcode = seq[i]
     return seq[0]
  

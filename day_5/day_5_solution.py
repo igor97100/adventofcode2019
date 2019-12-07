6ab53c745a6697c0b39b0f90d470b8e273750034
IgorSpi <igor13pao@hotmail.com>
2019-12-07 03:20:18 +0100

    added handling for opcode 3 and 4

--- day_5/day_5_solution.py
+++ day_5/day_5_solution.py
@@ -8,20 +8,24 @@ class Error(Exception):
 def main():
     input_ = np.loadtxt(fname = 'input', delimiter = ',', dtype=np.int64)
 
-def run_intcode(original_seq, noun, verb):
+
+def run_intcode(original_seq, only_input):
     seq = np.copy(original_seq)
-    seq[1] = noun
-    seq[2] = verb
-    for i in range(0, len(seq), 4):
-        opcode = seq[i]
-        if opcode == 99:
-            break
-        elif opcode == 1:
-            seq[seq[i+3]] = seq[seq[i+1]] + seq[seq[i+2]]
-        elif opcode == 2:
-            seq[seq[i+3]] = seq[seq[i+1]] * seq[seq[i+2]]
+    i = 0
+    opcode = seq[0]
+    while opcode != 99:
+        if opcode == 3:
+            seq[seq[i+1]] = only_input
+            i += 2
+        elif opcode == 4:
+            print(f'Opcode 4 output: {seq[seq[i+1]]}}')
+            i += 2
         else:
-            break
+
+            unmerged_opcode = map(int, str(opcode))
+            while len(unmerged_opcode != 5):
+                unmerged_opcode.insert(0, 0)
+        opcode = seq[i]
     return seq[0]
  
 if __name__ == '__main__':
